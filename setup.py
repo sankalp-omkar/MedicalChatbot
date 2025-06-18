287a48219495c02620207143f64ac7d49ce8a77a
sankalp-omkar <160780024+sankalp-omkar@users.noreply.github.com>
2025-06-18 18:14:18 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical ChatBot Project',
+    version= '0.0.0',
+    author= 'Sankalp Omkar',
+    author_email= 'sankalpomkar12@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
